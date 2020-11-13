e6ee20c1c4871691e20d46070e30eb40fbf388d1
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-11-12 19:41:50 -0600

    steps 1-2

---
+++ SpaDay/Controllers/Usercontroller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class Usercontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.User = newUser;
+                return View("Index");
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+    }
+}
