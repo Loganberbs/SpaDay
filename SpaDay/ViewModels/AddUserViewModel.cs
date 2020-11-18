e5f299eb6f9535649b8f15642a597f6591dcce72
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-11-17 20:25:06 -0600

    needs resolving

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {   
+        [Required(ErrorMessage = "Must Provide Username")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Must be between 5 and 15 characters")]
+        public string Username { get; set; }
+       
+        [EmailAddress(ErrorMessage = "Must have valid email address")]
+        public string Email { get; set; }
+        
+        [Required (ErrorMessage = "Must be between 6 and 20 characters")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "")]
+        public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Password must match")]
+        public string VerifyPassword { get; set; }
+    }
+}
